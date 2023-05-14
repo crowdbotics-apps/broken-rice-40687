df4126ca0963174fd5949264cfe8d76c6190efd4
Crowdbotics <team@crowdbotics.com>
2023-05-14 22:26:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-rice-40687",
+  url: "https://broken-rice-40687.botics.co",
+  api: "https://broken-rice-40687.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
